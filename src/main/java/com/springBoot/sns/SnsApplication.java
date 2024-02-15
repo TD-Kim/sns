77684a78828f32d94ae63471843ae5a3c0b0d412
TD-Kim <kjy.devops@gmail.com>
2024-02-15 13:51:05 +0900

    Update SnsApplication.java

--- src/main/java/com/springBoot/sns/SnsApplication.java
+++ src/main/java/com/springBoot/sns/SnsApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 
 public class SnsApplication {
-
+    // Ctrl + Alt + L : 코드 정렬
     public static void main(String[] args) {
         SpringApplication.run(SnsApplication.class, args);
     }
